fa83195a35813c567bac11ac39741e947ed8f009
Lucky Nkolonzi <35879963+nkolonzilucky@users.noreply.github.com>
2024-10-03 01:14:51 +0200

    Update auth.js

--- backend/utils/auth.js
+++ backend/utils/auth.js
@@ -8,4 +8,24 @@ function generateToken(userInfo) {
   return jwt.sign(userInfo, process.env.JWT_SECRET, {expiresIn: '1h'});
 }
 
-module.exports.generateToken = generateToken;
+function verifyToken(username, token) {
+  return jwt.verify(token, process.env.JWT_SECRET, (error, response) => {
+    if (error) {
+      return {
+        verified: false,
+        message: 'Invalid token'
+      }
+    }
+    if (response.username !== username) {
+      return {
+        verified: true,
+        message: 'verified'
+      }
+    }
+  });
+}
+
+module.exports = { 
+  generateToken,
+  verifyToken
+};
