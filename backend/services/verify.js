66cc3f469623aa39072bd1bcb66952314060f916
Lucky Nkolonzi <35879963+nkolonzilucky@users.noreply.github.com>
2024-10-03 01:19:05 +0200

    Update verify.js

--- backend/services/verify.js
+++ backend/services/verify.js
@@ -1,8 +1,9 @@
-const util = require('../utils/utils');
+const buildResponse = require('../utils/utils').buildResponse;
+const verifyToken = require('../utils/auth').verifyToken;
 
 function verify(requestBody) {
   if(!requestBody || !requestBody.user || !requestBody.user.username || !requestBody.token) {
-    return util.buildResponse(401, {
+    return buildResponse(401, {
       verified: false,
       message: 'incorrect request body'
     })
@@ -10,5 +11,17 @@ function verify(requestBody) {
 
   const user = requestBody.user;
   const token = requestBody.token;
-  const verification = 
+  const verification = verifyToken(user.username, token);
+  if(!verification.verified) {
+    return buildResponse(401, verification);
+  }
+
+  return buildResponse(200, {
+    verified: true,
+    message: 'success',
+    user: user,
+    token: token
+  }
 }
+
+module.exports.verify = verify;
