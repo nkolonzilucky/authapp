982d697a38785e5341960fff1d993e35890c0b3a
Lucky Nkolonzi <35879963+nkolonzilucky@users.noreply.github.com>
2024-10-03 18:14:49 +0200

    Update Register.js

--- frontend/src/Register.js
+++ frontend/src/Register.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+
 
 function Register() {
+    const [name, setName] = useState('');
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [message, setMessage] = useState(null);
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        if (username.trim() === '' || email.trim() === '' || name.trim() === '' || password.trim() === '') {
+            setMessage('All fields are required!')
+        }
+    }
+    
     return (
         <div>
-            <h1>Register</h1>
-            <p>Welcome to the Register page!</p>
+            <form onSubmit={submitHandler}>
+                <h5>Register</h5>
+                name: <input type="text" value={name} onChange={event => setName(event.target.value)} /> <br/>
+                email: <input type="text" value={email} onChange={event => setEmail(event.target.value)} /> <br/>
+                username: <input type="text" value={username} onChange={event => setUsername(event.target.value)} /> <br/>
+                password: <input type="password" value={password} onChange={event => setPassword(event.target.value)} /> <br/>
+                <input type="submit" value="Register" />                
+            </form>
+            {message && <p className="message">{message}</p>}
         </div>
     )
 };
