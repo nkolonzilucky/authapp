46400f0f7a38690a8615db27ad40a0a338b4439d
Lucky Nkolonzi <35879963+nkolonzilucky@users.noreply.github.com>
2024-10-03 19:20:13 +0200

    Update Login.js

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,10 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+const loginURL = '';
 
 function Login() {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [errorMessage, setErrorMessage] = useState(null);
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+        if(username.trim() === '' || password.trim() === '') {
+            setErrorMessage('Both username and password are required');
+            return;
+        }
+        setErrorMessage(null);
+        const requestConfig = {
+            headers: {'x-api-key': ''}
+        }
+        const requestBody = {
+            username: username,
+            passoword: password
+        }
+        axios.post(loginURL, requestBody, requestConfig).then(response => {
+            
+        })
+    } 
+    
     return (
         <div>
-            <h1>Login</h1>
-            <p>Welcome to the Login page!</p>
+           <form onSubmit={submitHandler} >
+            username: <input type="text" value={username} onChange={event => setUsername(event.target.value)} <br/>
+            password: <input type="password" value={password} onChange={event => setPassword(event.target.value)} <br/>
+            <input type="submit" value="login" />
+            </form>
+        {errorMessage && <p className='message'>{errorMessage}</p>}
         </div>
     )
 };
